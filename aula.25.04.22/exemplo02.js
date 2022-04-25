b844097a6bf51e2f57854beea435d6f8353c2b24
gabriela-aires <80225123+gabriela-aires@users.noreply.github.com>
2022-04-25 09:02:16 -0300

    Add files via upload

---
+++ aula.25.04.22/exemplo02.js
@@ -0,0 +1,7 @@
+var velocidade = 59
+console.log(`A velocidade do carro é de ${velocidade} Km/h`)
+if(velocidade>=60){
+    console.log(`Voce ultrapassou o limite de velocidade!`)
+    console.log(`voce sera multado!`)
+}
+console.log(`dirija com cuidado!`)
---
+++ aula.25.04.22/exemplo03.js
@@ -0,0 +1,8 @@
+var pais = 'EUA'
+console.log(`Vivendo em ${pais}`)
+if (pais=='BRA'){
+    console.log(`você é Brasileiro!`)
+
+}else{
+    console.log(`Você é estrangeiro!`)
+}
